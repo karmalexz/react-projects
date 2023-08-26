30eb0badd7d0d7071e069871b6eca2c97b549339
karmalexz <hellozhangdi1994@gmail.com>
2023-08-27 00:33:37 +1000

    counter version2, changed the step to a step slider, changed count to a input box and buttons to controll, and add an reset button, however reset cant reset the slider

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import Slider from '@mui/material/Slider';
+import Box from "@mui/material/Box";
 
 function App() {
   return <div className="App"><Counter /></div>;
@@ -8,33 +10,53 @@ function App() {
 function Counter() {
   let date = new Date();
   const [step, setStep] = useState(1);
+  const [newStep, setNewStep] = useState(1)
   const [count, setCount] = useState(0);
-  date.setDate(date.getDate() + count)
+  const [newCount, setNewCount] = useState(0)
+  date.setDate(date.getDate() + newCount)
 
   return <>
+
+
+    <Box sx={{ width: "30%", marginLeft: "auto", marginRight: "auto", marginTop: 4 }}>
+
+      <Slider
+        defaultValue={1}
+        // getAriaValueText={valuetext}
+        valueLabelDisplay="auto"
+        step={1}
+        marks
+        min={1}
+        max={20}
+        className="slider"
+        onChange={(e) => { setNewStep(e.target.value) }}
+      />
+      step:{newStep}
+
+    </Box>
+
+
     <div>
-      <button onClick={() => setStep(step - 1)}>
+      <button onClick={() => setNewCount(newCount - newStep)}>
         -
       </button>
-      Step:{step}
-      <button onClick={() => setStep(step + 1)}>
+
+      <input type="text" onChange={(e) => { setNewCount(Number(e.target.value)) }} defaultValue={newCount} value={newCount} />
+      <button onClick={() => setNewCount(newCount + newStep)}>
         +
       </button>
     </div>
 
     <div>
-      <button onClick={() => setCount(count - step)}>
-        -
-      </button>
-      Count:{count}
-      <button onClick={() => setCount(count + step)}>
-        +
-      </button>
+      {newCount === 0 ? `Today is ${date.toString().slice(0, 15)}` : newCount > 0 ? `${newCount} days from today is ${date.toString().slice(0, 15)}` : `${Math.abs(newCount)} days ago is ${date.toString().slice(0, 15)}`}
     </div>
 
-    <div>
-      {count === 0 ? `Today is ${date.toString().slice(0, 15)}` : count > 0 ? `${count} days from today is ${date.toString().slice(0, 15)}` : `${Math.abs(count)} days ago is ${date.toString().slice(0, 15)}`}
-    </div>
+    <button className={`${(newCount === 0 && newStep === 1) ? "button-block" : ""}`} onClick={() => {
+      setNewCount(0)
+      setNewStep(1)
+    }}>
+      reset
+    </button>
   </>
 }
 export default App;
